73209b038065aa1d6f7b0a2c971e9b30a28c00a0
Göktuğ Tunç <49368916+goktugtunc@users.noreply.github.com>
2023-09-07 01:35:48 +0300

    Dosyalardan silinen fonksiyonlar silindi!

--- push_swap.h
+++ push_swap.h
@@ -31,20 +31,19 @@ typedef struct s_list{
 }	t_list;
 
 void	print(char *a);
-int		ft_isdigit(char c);
+int	ft_isdigit(char c);
 void	argstotext(t_list *data, char **args);
-int		errorcontrol1(char **args);
-int		errorcontrol2(t_list *data);
+int	errorcontrol1(char **args);
+int	errorcontrol2(t_list *data);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	**ft_split(char const *s, char c);
 long	ft_atol(char *a);
-int		errorcontrol3(t_list *data);
+int	errorcontrol3(t_list *data);
 void	freestruct(t_list *data);
 void	placea(t_list *data);
-//int		threealgorithm(t_list *data);
 void	revrotatea(t_list *data);
 void	revrotateb(t_list *data);
 void	revrotater(t_list *data);
@@ -60,8 +59,6 @@ void	ft_downone_a(t_list *data);
 void	ft_upone_a(t_list *data);
 void	ft_downone_b(t_list *data);
 void	ft_upone_b(t_list *data);
-int		radix_sort(t_list *data);
-void	ft_getmaxbits(t_list *data);
 void	algorithmtwo(t_list *data);
 
 #endif
